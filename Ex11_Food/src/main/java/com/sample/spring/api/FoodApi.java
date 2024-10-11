0c46b0dc1b268bbdb885f65620bf2dc6d47c1904
OhJeongHwan1 <wjdghks0316@naver.com>
2024-10-11 14:33:35 +0900

    10/11

--- Ex11_Food/src/main/java/com/sample/spring/api/FoodApi.java
+++ Ex11_Food/src/main/java/com/sample/spring/api/FoodApi.java
@@ -1,31 +1,50 @@
 package com.sample.spring.api;
 
 import com.sample.spring.api.request.CreateAndEditFoodRequest;
+import com.sample.spring.api.response.FoodDetailView;
+import com.sample.spring.model.FoodEntity;
+import com.sample.spring.repository.FoodRepository;
+import com.sample.spring.api.response.FoodView;
+import com.sample.spring.service.FoodService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.ZonedDateTime;
+import java.util.List;
 
 @RestController
 public class FoodApi {
+    @Autowired
+    private FoodService foodService;
+    @Autowired
+    private FoodRepository foodRepository;
+
     @GetMapping("/foods")
-    public String getfoods() {
-        return "getFoods";
+    public List<FoodView> getfoods() {
+        return foodService.getAllFoods();
     }
     @GetMapping("/food/{foodId}")
-    public String viewFood(@PathVariable("foodId") Long foodId) {
-        return "viewFood " + foodId;
+    public FoodDetailView viewFood(@PathVariable("foodId") Long foodId) {
+        return foodService.getFoodDetail(foodId);
     }
     @PostMapping("/food")
-    public String postFood(@RequestBody CreateAndEditFoodRequest request){
-        return "postFood / name: " + request.getName() + ", address: " + request.getAddress();
+    public FoodEntity postFood(@RequestBody CreateAndEditFoodRequest request){
+        return foodService.createFood(request);
+//        return "postFood / name: " + request.getName() + ", address: " + request.getAddress() + ", 메뉴[0]" + request.getMenus().get(1).getName();
     }
     @PutMapping("/food/{foodId}")
-    public String putFood(
+    public void editFood(
             @PathVariable("foodId") Long foodId,
             @RequestBody CreateAndEditFoodRequest request
     ) {
-        return "editFood " + foodId + ",name: " + request.getName() + ", address: " + request.getAddress();
+
+        foodService.editFood(foodId, request);
+//        return "editFood " + foodId + ",name: " + request.getName() + ", address: " + request.getAddress();
     }
     @DeleteMapping("/food/{foodId}")
-    public String deleteFood(@PathVariable("foodId") Long foodId) {
-        return "deleteFood " + foodId;
+    public void deleteFood(@PathVariable("foodId") Long foodId) {
+        foodService.deleteFood(foodId);
+
+//        return "deleteFood " + foodId;
     }
 }
